fff0ebe9f1a6fbd9d69dac44a72c34b11e7c2bbb
da-okazaki <35363213+da-okazaki@users.noreply.github.com>
2019-12-21 13:45:06 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class Mail extends React.Component {
+  render(props) {
+    return (
+      <div>
+        <h1>Hello {this.props.mail}</h1>
+        <input />
+      </div>
+    );
+  }
+}
+
+const Language = props => {
+  return <h1>Hello {props.name}</h1>;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Language name="World" />
+      <Mail mail="mail" />
+      <div>
+        <input type="submit" value="送信" />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
