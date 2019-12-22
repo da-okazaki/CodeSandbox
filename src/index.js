823fb07284b2bdfb0d9eefb1fe724e5d5d845bb2
da-okazaki <35363213+da-okazaki@users.noreply.github.com>
2019-12-22 12:22:18 +0900

    [update] コメントアウト削除

--- src/index.js
+++ src/index.js
@@ -7,22 +7,3 @@ import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-/* 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <ul>
-        <li>ラーメン</li>
-        <li>焼肉</li>
-        <li>寿司</li>
-      </ul>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-*/
